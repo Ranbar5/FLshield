dba1e1b70383cfcaac6afb7e79d4ad35b38e0550
Soporte <randall.ranbar5@gmail.com>
2026-06-29 12:45:50 -0600

    Remove admin extras bundle from QR config

--- backend/server.py
+++ backend/server.py
@@ -881,9 +881,6 @@ async def get_enrollment_config(
         "android.app.extra.PROVISIONING_DEVICE_ADMIN_SIGNATURE_CHECKSUM": signature_checksum,
         "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_DOWNLOAD_LOCATION": download_url,
         "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_CHECKSUM": package_checksum,
-        "android.app.extra.PROVISIONING_ADMIN_EXTRAS_BUNDLE": {
-            "server_url": server_url
-        }
     }
     
     if wifi_ssid:
